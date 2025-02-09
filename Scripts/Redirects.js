5d87c5ddc46ed8693420e01ab75e212029287742
AlienBlox <alienthebest123@outlook.com>
2025-02-09 17:55:20 +0000

    ok

---
+++ Scripts/Redirects.js
@@ -0,0 +1,18 @@
+function RedirectSteam()
+{
+	window.location.href = "https://steamcommunity.com/sharedfiles/filedetails/?id=3152490542";
+}
+
+function RedirectLogs()
+{
+	window.location.href = "https://steamcommunity.com/sharedfiles/filedetails/changelog/3152490542";
+}
+
+function Redirect(lnk)
+{
+	window.location.href = lnk;
+}
+
+function RedirectYouTube() {
+	window.location.href = "https://www.youtube.com/channel/UCpu_V3nxWViuAOHAeGlkyvQ/";
+}
